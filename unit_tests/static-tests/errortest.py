9d7679b3e0c31d1542fdc3f35bde38c1ba0ae4e0
Marcus Rowe <undisbeliever@gmail.com>
2016-11-09 20:25:23 +1000

    fix: update errortest script for bass v14.05

--- unit_tests/static-tests/errortest.py
+++ unit_tests/static-tests/errortest.py
@@ -20,16 +20,27 @@ def searchErrorLine(filename):
 def doTest(filename):
     process = subprocess.Popen(
         ("bass", "-strict", "-o", os.devnull, filename), 
-        stdout=subprocess.PIPE)
-    out = process.communicate()[0].decode('utf-8')
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    pout, perr = process.communicate()
+    pout = pout.decode('utf-8')
+    perr = perr.decode('utf-8')
 
     if process.returncode == 0:
         raise Exception(filename + ": bass did not fail")
 
     errorLine = searchErrorLine(filename)
 
-    lastError = out.splitlines()[-2]
-    match = re.match("   " + re.escape(filename) + r":(\d+):", lastError)
+    # have to test both stderr and stdout as bass-ARM9 and bass-v14.05
+    # output errors to different streams.
+
+    regex = re.compile(r"^\s*" + re.escape(filename) + r":(\d+):", re.MULTILINE)
+
+    match = None
+    for match in regex.finditer(pout):
+        pass
+    for match in regex.finditer(perr):
+        pass
 
     if match is None or int(match.group(1)) != errorLine:
         raise Exception(filename + ": did not error out on marked line")
