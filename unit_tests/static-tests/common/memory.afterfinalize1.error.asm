73e4420dca636e56d2fefd7eaf2db5d5271be035
Marcus Rowe <undisbeliever@gmail.com>
2017-04-28 21:31:23 +1000

    feat: storeBlockUsed and storeBlockCount memory macros

---
+++ unit_tests/static-tests/common/memory.afterfinalize1.error.asm
@@ -0,0 +1,17 @@
+
+define MEMORY_MAP(LOROM)
+define ROM_SIZE(1)
+
+include "../../../src/common/memory.inc"
+
+createCodeBlock(code,  0xc08000, 0xc0ffaf)
+createDataBlock(rom1,  0xc18000, 0xc1ffff)
+
+createRamBlock(dp,     0x000000, 0x0000ff)
+createRamBlock(shadow, 0x7e0100, 0x7e1f7f)
+createRamBlock(stack,  0x7e1f80, 0x7e1fff)
+
+finalizeMemory()
+
+createDataBlock(rom2,  0xc28000, 0xc2ffff) // ERROR
+
--- unit_tests/static-tests/common/memory.rom.success.asm
+++ unit_tests/static-tests/common/memory.rom.success.asm
@@ -10,8 +10,8 @@ include "../../../src/common/memory.inc"
 
 createCodeBlock(code,  0xc08000, 0xc0ffaf)
 createDataBlock(rom1,  0xc18000, 0xc1ffff)
-createDataBlock(rom2,  0xc28000, 0xc2efff)
 
+createDataBlock(addrTable, 0xc28000, 0xc2efff)
 createDataBlock(fixedsize, 0xc2f000, 0xc2ffff)
 
 code(code)
@@ -20,7 +20,7 @@ assert(pc() == 0xc08000)
 rodata(rom1)
 assert(pc() == 0xc18000)
 
-rodata(rom2)
+rodata(addrTable)
 assert(pc() == 0xc28000)
 
 scope testInScope {
@@ -46,6 +46,19 @@ rodata(rom1)
 assert(pc() == 0xc18004)
 
 
+scope AddrTableData {
+    rodata(addrTable)
+
+    constant N_ITEMS(15)
+    constant ELEMENT_SIZE(5)
+
+    variable n(0)
+    while n < N_ITEMS {
+        fill ELEMENT_SIZE, n
+        n = n + 1
+    }
+}
+
 rodata(fixedsize)
 fill 0x1000, 0xff
 code()
@@ -54,8 +67,29 @@ code()
 storeBlockStart(rom1Start, rom1)
 assert(rom1Start == 0xc18000)
 
-storeBlockStart(rom2Start, rom2)
-assert(rom2Start == 0xc28000)
+storeBlockStart(addrTableStart, addrTable)
+assert(addrTableStart == 0xc28000)
+
+
+finalizeMemory()
+
+
+storeBlockUsed(codeUsed, code)
+assert(codeUsed == 2)
+
+storeBlockUsed(rom1Used, rom1)
+assert(rom1Used == 4)
+
+storeBlockUsed(addrTableUsed, addrTable)
+assert(addrTableUsed == AddrTableData.N_ITEMS * AddrTableData.ELEMENT_SIZE)
+
+storeBlockUsed(fixedsizeUsed, fixedsize)
+assert(fixedsizeUsed == 0x1000)
+
+
+storeBlockCount(addrTableCount, addrTable, AddrTableData.ELEMENT_SIZE)
+assert(addrTableCount == AddrTableData.N_ITEMS)
+
 
 // vim: ft=bass-65816 ts=4 sw=4 et:
 
