e13d2278a4080f5ad5f2ebd46220ba66ce291c65
Marcus Rowe <undisbeliever@gmail.com>
2017-05-17 21:00:53 +1000

    chore: add copyright/license preamble to source files

--- tables/entityhitbox-collisionorder.py
+++ tables/entityhitbox-collisionorder.py
@@ -6,6 +6,10 @@
 Entity Hithox collision order table generator.
 """
 
+# This file is part of the UnTech Game Engine.
+# Copyright (c) 2016 - 2017, Marcus Rowe <undisbeliever@gmail.com>.
+# Distributed under The MIT License: https://opensource.org/licenses/MIT
+
 
 MAX_INNER_HITBOXES = 4
 LIST_SHIFT = 2
