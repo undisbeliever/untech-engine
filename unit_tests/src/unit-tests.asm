ea37786584693da4a93a145a11cfd5c2025956f0
Marcus Rowe <undisbeliever@gmail.com>
2018-11-12 12:35:53 +1000

    Rewrite delete entities in list routines
    The following has been removed:
     * Entity.ClearActivateNextFrameList routine
     * Entity._DeleteAllInList macro
    
    The following routines have been rewritten:
     * Entity.DeleteDeactivatedEntities
    
    The following routines have been added:
     * Entity.DeleteEntitiesInList
     * Entity.DeleteEntitiesInActivateNextFrameList

--- unit_tests/src/unit-tests.asm
+++ unit_tests/src/unit-tests.asm
@@ -47,6 +47,7 @@ include "tests/entity/entities/delete-if-outside.inc"
 
 include "tests/entity/_common.inc"
 include "tests/entity/allocation.inc"
+include "tests/entity/deallocation.inc"
 include "tests/entity/counters.inc"
 include "tests/entity/gameloop.inc"
 include "tests/entity/spawn.inc"
