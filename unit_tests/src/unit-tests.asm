00370446bf95fc342b629ff1b36aa8375a1edc8e
Marcus Rowe <undisbeliever@gmail.com>
2020-07-14 00:14:31 +1000

    Add interactive tiles to the engine

--- unit_tests/src/unit-tests.asm
+++ unit_tests/src/unit-tests.asm
@@ -79,6 +79,7 @@ include "tests/text/string.inc"
 
 include "tests/util/lz4.inc"
 
+include "interactive/interactive-tiles-test.inc"
 include "interactive/tile-collision-test.inc"
 include "interactive/entity-validation.inc"
 include "interactive/entityhitbox-collisions.inc"
